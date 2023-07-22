590869ae7304cc4c0a6cdf658a5c37feb272fd98
Joao Vitor Sampaio <joaovitorsampaio001@hotmail.com>
2023-07-22 12:03:37 -0300

    Search Customer

--- src/Assets/Pages/HomePage/HomePage.jsx
+++ src/Assets/Pages/HomePage/HomePage.jsx
@@ -1,27 +1,41 @@
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getDataBase } from "../../Redux/Reducers/GetDataBase/Components/AsyncThunk";
 
 function HomePage() {
   const dispatch = useDispatch();
   const customers = useSelector((state) => state.getAll.data);
+  const [search, setSearch] = useState("");
 
   useEffect(() => {
     dispatch(getDataBase());
   }, [dispatch]);
+
+  const filteredCustomers = customers.filter(
+    (customer) =>
+      customer.fullName.toLowerCase().includes(search.toLowerCase()) ||
+      customer.email.toLowerCase().includes(search.toLowerCase())
+  );
+
   return (
-    <>
-      <div>
-        <ul>
-          {customers.map((customer) => (
-            <li key={customer.id}>
-              <p>Name: {customer.fullName}</p>
-              <p>Email: {customer.email}</p>
-            </li>
-          ))}
-        </ul>
-      </div>
-    </>
+    <div>
+      <input
+        type="text"
+        placeholder="Search Customer"
+        value={search}
+        onChange={(event) => {
+          setSearch(event.target.value);
+        }}
+      />
+      <ul>
+        {filteredCustomers.map((customer) => (
+          <li key={customer.id}>
+            <p>Name: {customer.fullName}</p>
+            <p>Email: {customer.email}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 }
 
