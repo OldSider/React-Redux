5b45b4903dea37c26552e09d4ca1bb81ddf58cbf
Joao Vitor Sampaio <joaovitorsampaio001@hotmail.com>
2023-07-22 21:56:02 -0300

    style homepage

--- src/Assets/Pages/HomePage/HomePage.jsx
+++ src/Assets/Pages/HomePage/HomePage.jsx
@@ -2,6 +2,8 @@ import { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getDataBase } from "../../Redux/Reducers/GetDataBase/Components/AsyncThunk";
 
+import "./Components/Style/HomePage.css";
+
 function HomePage() {
   const dispatch = useDispatch();
   const customers = useSelector((state) => state.getAll.data);
@@ -18,24 +20,46 @@ function HomePage() {
   );
 
   return (
-    <div>
-      <input
-        type="text"
-        placeholder="Search Customer"
-        value={search}
-        onChange={(event) => {
-          setSearch(event.target.value);
-        }}
-      />
-      <ul>
-        {filteredCustomers.map((customer) => (
-          <li key={customer.id}>
-            <p>Name: {customer.fullName}</p>
-            <p>Email: {customer.email}</p>
-          </li>
-        ))}
-      </ul>
-    </div>
+    <>
+      <div className="wrapper">
+        <div className="div-btn">
+          <input
+            type="text"
+            placeholder="Search Customer"
+            value={search}
+            onChange={(event) => {
+              setSearch(event.target.value);
+            }}
+            className="header-filter"
+          />
+
+          <button className="header-btnAdd">Adicionar</button>
+          <button className="header-btn">Sair</button>
+        </div>
+        <table>
+          <thead>
+            <tr>
+              <th>Id</th>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {filteredCustomers.map((customer) => (
+              <tr key={customer.id}>
+                <td>{customer.id}</td>
+                <td>{customer.fullName}</td>
+                <td>{customer.email}</td>
+                <td>
+                  <button className="btn1">view</button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </>
   );
 }
 
